1ea69cb8bff3a36a8aa8745e252507d45e28f824
SWilDeV <sean0669@gmail.com>
2022-06-27 22:43:51 +0200

    Composition API 1

--- UltimateTeamFront/.eslintrc.cjs
+++ UltimateTeamFront/.eslintrc.cjs
@@ -1,11 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended"
+    // "eslint:recommended"
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+};
--- UltimateTeamFront/src/views/TournoisListView.vue
+++ UltimateTeamFront/src/views/TournoisListView.vue
@@ -1,8 +1,38 @@
-<script setup>
+<script>
   import { getTournaments } from "../apiVue";
-  getTournaments().then((response) => {
-    this.tournaments = response.data;
+  import { ref, defineComponent } from "vue";
+  export default defineComponent({
+    setup() {
+      let tournament = ref(null);
+      try {
+        const tournament = getTournaments().then((response) => {
+          const data = response.data;
+          console.log(data);
+          return data;
+        });
+      } catch (e) {
+        console.log(e);
+      }
+
+      return { tournament };
+    },
   });
+
+  // const tournament = getTournaments().then((response) => {
+  //   this.tournaments = response.data;
+  //   return Promise.resolve(response.data);
+  // });
+  // console.log(tournament);
+
+  // const tournament = ref(null);
+  // async function tourn(){
+  //   try{
+  //     const tournament = await getTournaments()
+  //   }
+  //   catch (e) {
+  //     console.log(e)
+  //   }
+  // }
 
   // onMounted(() => {
   //   test();
@@ -15,6 +45,6 @@
 <template>
   <div>
     <h1>TournoiListViews</h1>
-    <!-- <div>{{ tournaments[0].nomTournoi }}</div> -->
+    <!-- <div>{{ tournament }}</div> -->
   </div>
 </template>
