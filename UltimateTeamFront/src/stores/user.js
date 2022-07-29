cc68828997d332315b3b42fad21c8fdd24c4cf91
SWilDeV <sean0669@gmail.com>
2022-07-29 14:02:24 +0200

    router in pinia ok

--- UltimateTeamFront/src/stores/user.js
+++ UltimateTeamFront/src/stores/user.js
@@ -1,10 +1,5 @@
 import { defineStore } from "pinia";
 import { registerUser } from "../apiVue";
-// import { router } from "@/router";
-// import { useRouter } from "vue-router";
-// import { router } from "vue-router";
-
-// const router = useRouter();
 export const useUserStore = defineStore({
   id: "user",
   state: () => ({
@@ -15,10 +10,9 @@ export const useUserStore = defineStore({
     async register(form) {
       try {
         await registerUser(form).then((response) => {
-          //const router = useRouter();
           this.user = response.data;
           localStorage.setItem("user", JSON.stringify(response.data));
-          // router.push({ name: "login" });
+          this.$router.push({ name: "login" });
         });
       } catch (e) {
         console.log(e);
